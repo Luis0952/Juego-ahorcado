4c372f0e446a32f715dda1803e345f09543d5427
Luis Rosales <luchorosalesc6@gmail.com>
2022-09-09 11:24:31 -0500

    Se agregó un archivo .ts en donde se puso un arreglo de las palabras que se van a usar

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,33 @@
 
+import { useState } from 'react';
 import HangImage from './components/HangImage'
 import { letras } from './helpers/letras'
+import { obtenerPalabras } from './helpers/obtenerPalabras';
 
 
 function App() {
 
+  const [palabras, setPalabras] = useState(obtenerPalabras)
+  const [intentos, setIntentos] = useState(0);
+
+  //Validacion de las palabras
+  const validarPalabras=(letra: string)=>{
+      console.log(letra)
+  }
+
+
   return (
   <div className="text-center">
 
-    <HangImage/>
+   <HangImage imagenNumero={3}/>
+      
+     
 
       <h3 
       className="font-bold text-4xl my-4 text-blue-600"> _ _ _ _ _ _ </h3>
       <h3 
       className="font-bold text-2xl my-4 text-blue-600">
-      Intento: 0 </h3>
+      Intento: {intentos} </h3>
      {/* Mensaje si perdió */}       
       <h3 
       className="font-bold text-2xl my-4 text-red-600">
@@ -25,8 +38,12 @@ function App() {
         🥳 Felicdades, usted Ganó! </h3> 
      {/* Teclado */}      
      {letras.map((letra) => ( 
-       <button className="bg-white p-4 rounded-full shadow-md m-2" 
-           key={letra}>{letra}</button>  
+       <button 
+      className="bg-white p-4 rounded-full shadow-md m-2" 
+      key={letra}
+      onClick={()=>validarPalabras(letra)}//En el onClick tiene que ir como función
+      >{letra}
+      </button>  
       ))}
 
       {/* Boton de nuevo juego */}       
---
+++ src/helpers/obtenerPalabras.ts
@@ -0,0 +1,17 @@
+let palabras: string[] = [     
+    "BONDAD",
+    "SINCERIDAD",
+    "EMPATIA",
+    "AMOR",
+    "PACIENCIA",
+    "GRATITUD",
+    "HUMILDAD",
+    "PERDON",
+    "RESPONSABILIDAD",
+    "SOLIDARIDAD"   
+];
+
+export const obtenerPalabras=()=>{
+    const randomIndex=Math.floor(Math.random()*palabras.length) 
+    return 'Hola';
+}
